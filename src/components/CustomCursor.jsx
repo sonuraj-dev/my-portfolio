7d16b9d76d9cd551ae8c3833d607b8d0a1064e0d
sonuraj-dev <sonuraj9631sonu@gmail.com>
2026-02-18 22:39:39 +0530

    day_7_3

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react"
 
-
+// Custom cursor component that follows the mouse movement and displays a blurred circle just like the one in the Home section background. It uses the useState hook to track the cursor position and the useEffect hook to add and remove the mousemove event listener. The cursor is styled with Tailwind CSS classes to create a gradient, blurred circle that follows the mouse pointer.
 
 export default function CustomCursor(){
   
