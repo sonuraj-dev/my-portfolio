e6a6ee404fca598c63efb9cd708ee1d7919b86bf
sonuraj-dev <sonuraj9631sonu@gmail.com>
2026-02-22 22:11:04 +0530

    day_10_c1

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -93,7 +93,7 @@ export default function ParticlesBackground(){
 
     <canvas
     ref={canvasRef}
-     className="fixed top-0 left-0 w-full h-full pointer-events-none z-0"
+     className="absolute top-0 left-0 w-full h-full pointer-events-none z-0"
     
     ></canvas>
   )
