3c6bb198f16b389e95fcfcd5b7be3079fa4bbb94
sonuraj-dev <sonuraj9631sonu@gmail.com>
2026-02-22 22:17:03 +0530

    day_10_c3

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,2 +1,7 @@
 export default function About(){
   return (
+    <section id="about" className="min-h-screen w-full flex items-center justify-center relative bg-black text-white overflow-hidden">
+
+      </section>
+  )
+}
