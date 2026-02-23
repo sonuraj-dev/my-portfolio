92192997be6e862897ed93a432e7e9d1147be663
sonuraj-dev <sonuraj9631sonu@gmail.com>
2026-02-23 22:03:13 +0530

    day_10_cex

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,12 @@
 export default function About(){
   return (
-    <section id="about" className="min-h-screen w-full flex items-center justify-center relative bg-black text-white overflow-hidden">
-
+    <section id="about" 
+    className="min-h-screen w-full flex items-center justify-center relative bg-black text-white overflow-hidden">
+      
       </section>
-  )
+  
+)
 }
+
+
+//TOMORROW  i will do rest\
